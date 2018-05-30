f7661d317779cdff7acc831ddabbe6cd8d9d972b
Kwilkerson723 <kwilkerson723@gmail.com>
2018-05-30 11:16:25 -0700

    Update Chapter 5-9 #10.asm

--- Chapter 5/Chapter 5-9 #10.asm
+++ Chapter 5/Chapter 5-9 #10.asm
@@ -1,24 +1,24 @@
 ExitProcess proto
+WriteInt64 proto
+Crlf proto
 .data
 sum qword 0
 n qword 47
+
 .code
 main proc
- mov rdi,0
- mov rcx,47
- mov rbx,0
- mov rax,1
-
-
- L1:
-
- mov rdx,rbx
- add rbx,rax
- mov rax,rdx
- mov sum[rdi],rbx
-
- loop L1
-
+	mov rdi,1
+	mov rcx,47
+	mov rbx,0
+	mov rax,0
+L1:
+	mov rdx,rbx
+	add rbx,rdi
+	mov rdi,rdx
+	mov rax,rbx
+	call WriteInt64
+ call Crlf
+loop L1
 
 
    call ExitProcess
